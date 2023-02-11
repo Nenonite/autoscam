b325db06525de1b27f822f165170a6f1def5355c
Nenonite <75539862+Nenonite@users.noreply.github.com>
2023-02-11 23:13:37 +0300

    фиксы кода...

--- main.py
+++ main.py
@@ -18,28 +18,39 @@ def start(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     chatid = call.message.json["chat"]["id"] #колхоз для получения айди чата (правда бот по идее будет работать везде, даже не в лс)
+    msgid = call.message.message_id
     if call.data == "ru":
             markup = types.InlineKeyboardMarkup()
             btn1 = types.InlineKeyboardButton("Заскамиться", callback_data="scam")
             btn2= types.InlineKeyboardButton(text='Канал', url='https://t.me/lavkaton')
             btn3= types.InlineKeyboardButton(text='Сайт', url='https://lavkafoundation.fun')
             btn4= types.InlineKeyboardButton(text='Github', url='https://github.com/lavkacoin/autoscam')
-            btn5 = types.InlineKeyboardButton('🔙 Вернуться к выбору языка', callback_data="languageswtich")
+            btn5 = types.InlineKeyboardButton('🔙 Вернуться к выбору языка', callback_data="languageswitch")
             markup.row(btn1)
             markup.row(btn2, btn3)
             markup.row(btn4)
             markup.row(btn5)
-            bot.send_message(chatid, "Добро пожаловать в LAVKA autoscam. \n \n LAVKA autoscam это открытая технология для скама. Если раньше вам приходилось покупать сомнительные NFT чтобы заскамиться, ждать скамеров - теперь вы можете просто нажать кнопку ниже. \n \n А так же можете посмотреть открытый исходный код этой технологии + подписаться на LAVKA Foundation", reply_markup=markup)
+            bot.edit_message_text("Добро пожаловать в LAVKA autoscam. \n \n LAVKA autoscam это открытая технология для скама. Если раньше вам приходилось покупать сомнительные NFT чтобы заскамиться, ждать скамеров - теперь вы можете просто нажать кнопку ниже. \n \n А так же можете посмотреть открытый исходный код этой технологии + подписаться на LAVKA Foundation",chatid, msgid, parse_mode='HTML', disable_web_page_preview=True, reply_markup=markup)
             try:            
-                bot.answer_callback_query(call)
-            except: 
-            	print("шашыпка пофиг работаем дальше")
+                bot.answer_callback_query(call.id)
+            except Exception as e:
+            	print(f"шашыпка пофиг работаем дальше {e}")
     elif call.data == "en":
-            bot.send_message(chatid, "Yes/no?")
-            try:            
-                bot.answer_callback_query(call)
-            except: 
+           bot.send_message(chatid, "Yes/no?")
+           try:            
+                bot.answer_callback_query(call.id)
+           except: 
             	print("шашыпка пофиг работаем дальше")
+    elif call.data == "languageswitch":
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton("🇷🇺 Русский", callback_data="ru")
+        btn2 = types.InlineKeyboardButton('🇬🇧 English', callback_data="en")
+        markup.add(btn1, btn2)
+        bot.edit_message_text("🇷🇺 Выберите язык / 🇬🇧 Choose your language", chatid, msgid, reply_markup=markup)
+        try:            
+            bot.answer_callback_query(call.id)
+        except: 
+            print("шашыпка пофиг работаем дальше")
 
 
 bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
